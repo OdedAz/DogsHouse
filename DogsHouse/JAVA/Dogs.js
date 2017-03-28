13e188499fcc6f6709311f888d570a5fb4fc6f9b
Oded Azoulay <Oded Azoulay>
2017-03-29 02:27:08 +0300

    uploading new files

---
+++ DogsHouse/JAVA/Dogs.js
@@ -0,0 +1,38 @@
+﻿
+var dog = (function () {
+
+    var Name;
+    var Type;
+    var id;
+    var dateOfBirth;
+    var sex;
+    var mother;
+    var father;
+    var buyerId;
+    var BuyerName;
+
+
+    function dogCTOR(i_dateOfBirth, i_Name, i_Type, i_id, i_sex, i_father, i_mather,i_buyerId) {
+        Name = i_Name;
+        Type = i_Type;
+        id = i_id;
+        dateOfBirth = i_dateOfBirth;
+        sex = i_sex;
+        father = i_father;
+        mother = i_mather;
+        BuyerName = "None";
+        buyerId = i_buyerId;
+        
+    }
+
+    dogCTOR.prototype.addDog = function (i_Param1) {
+        print("please enter the Name of the dog");
+        
+        
+    };
+
+    return dogCTOR;
+
+})();
+
+var Dog = "spaike";
---
+++ DogsHouse/JAVA/newDog.js
@@ -0,0 +1,12 @@
+﻿function dogGender()
+{
+    var x;
+    x = document.getElementById('sex').value;
+    if (x!='m'& x!='M'& x!='F'& x!='f')
+        alert("the dog sex input is wrong");
+    }
+
+function dogType() {
+    var x;
+    x = docu
+}
